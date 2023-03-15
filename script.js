c543ee9c9dd8712d6838f4c3ac9e19f025d50c04
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-15 11:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+function ResolvePromise(){
+	return new Promise((resolve,reject)=>{
+		resolve(setTimeout(()=>{
+			output.innerHTML = 'Hello, world!'
+		} , 1000))
+	})
+}
